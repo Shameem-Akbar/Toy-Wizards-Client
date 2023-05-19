a28023f91a71b2ae9d6579cff20d2f483ec36ac0
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-05-19 12:51:27 +0600

    scroll restoration and progress bar on loading added

--- src/layout/LoginLayout/LoginLayout.jsx
+++ src/layout/LoginLayout/LoginLayout.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import NavigationBar from '../../pages/shared/NavigationBar/NavigationBar';
 import Footer from '../../pages/shared/Footer/Footer';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const LoginLayout = () => {
+    const { loading } = useContext(AuthContext);
+    if (loading) {
+        return <div className='flex justify-center mt-48'><progress className="progress progress-primary w-56"></progress></div>
+    }
+
     return (
         <div>
             <NavigationBar></NavigationBar>
             <Outlet></Outlet>
             <Footer></Footer>
+            <ScrollRestoration></ScrollRestoration>
         </div>
     );
 };
--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import NavigationBar from '../../pages/shared/NavigationBar/NavigationBar';
 import Footer from '../../pages/shared/Footer/Footer';
 import PageTitle from '../../components/PageTitle/PageTitle';
 import Home from '../../pages/Home/Home/Home';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Main = () => {
+    const { loading } = useContext(AuthContext);
+    if (loading) {
+        return <div className='flex justify-center mt-48'><progress className="progress progress-primary w-56"></progress></div>
+    }
     return (
         <div>
             <PageTitle title="Home"></PageTitle>
             <NavigationBar></NavigationBar>
             <Home></Home>
             {/* <Footer></Footer> */}
+            <ScrollRestoration></ScrollRestoration>
         </div>
     );
 };
